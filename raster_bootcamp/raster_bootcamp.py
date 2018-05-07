4a0c83dbe27304bc249208653cec90d8487a301b
agnieszkajaniak <agnieszka.janiakk@gmail.com>
2018-05-07 21:44:16 +0200

    calculator adding

--- raster_bootcamp/raster_bootcamp.py
+++ raster_bootcamp/raster_bootcamp.py
@@ -2,7 +2,7 @@ import os
 from osgeo import gdal
 from os.path import basename, splitext
 
-os.chdir(os.path.join(os.path.expanduser("~"), "Documents", "dist"))
+os.chdir(os.path.join(os.path.expanduser("~"), "Documents", "pg", "raster_bootcamp", "dist"))
 lista = os.listdir(".")
 
 
@@ -14,42 +14,57 @@ class rast:
             raster = gdal.Open(r)
             self.rasters.append(raster)
             print (raster == None)
-            self.metadata.append(self.__getMetadata(raster))
+            self.metadata.append(self._getMetadata(raster))
+            self.temp = raster.GetRasterBand(1).ReadAsArray()
+        self.names = self._getNames(*lista)
+        self.result = np.zeros(self.size[0])
+        self.result.shape = (1, self.size[0])
+        self._summary()
+        self._calculator
 
-        self.names = self.__getNames(*lista)
-        # self.names = None
-        self.__summary()
-
-    def __getMetadata(self, raster):
-        geoTransform = raster.GetGeoTransform()
+    def _getMetadata(self, raster):
+        self.geoTransform = raster.GetGeoTransform()
         self.size = (raster.RasterXSize, raster.RasterYSize)
         self.projection = raster.GetProjection()
-        self.resolution = (abs(geoTransform[1]), abs(geoTransform[5]))
-        minX = geoTransform[0]
-        maxY = geoTransform[3]
-        maxX = minX + geoTransform[1] * self.size[0]
-        minY = maxY + geoTransform[5] * self.size[1]
+        self.resolution = (abs(self.geoTransform[1]), abs(self.geoTransform[5]))
+        minX = self.geoTransform[0]
+        maxY = self.geoTransform[3]
+        maxX = minX + self.geoTransform[1] * self.size[0]
+        minY = maxY + self.geoTransform[5] * self.size[1]
         self.extent = (minX, maxX, minY, maxY)
         return (self.size, self.projection, self.resolution, self.extent)
 
-    def __getNames(self, *lista):
+    def _getNames(self, *lista):
         return ([basename(splitext(raster)[0]) for raster in lista])
 
     def _getLines(self, row):
         self.__data = np.empty([len(self.rasters), self.size[0]])
-        for ptr in self.rastry:
+        for ptr in self.rasters:
             p = ptr.GetRasterBands(1).readAsArray(0, row, self.size[0], 1)
-            for i in range(len(rastry)):
+            for i in range(len(rasters)):
                 self.__data[i] = p
 
-    def __calc(self, expression):
+    def _calc(self, expression):
         dct = dict(zip(self.names, self.__data))
-        self.result = np.zeros(size[0])
-        self.result.shape = (1, size[0])
         self.result[0] = eval(expression, dct)
 
-    def __summary(self):
+    def _summary(self):
         print("Name:{0:s}\n".format(self.names))
 
-    def __calculator(self):
-        pass
+    def _calculator(self, expression):
+        cols, rows = self.size
+        driver = gdal.GetDriverByName("GTiff")
+        target = driver.Create("result.tif", cols, rows, 1, gdal.GDT_Byte)
+        target.SetProjection(self.projection)
+        target.SetGeoTransform(self.geoTransform)
+        noData = 255
+        tband = target.GetRasterBand(1)
+        tband.SetNoDataValue(noData)
+        for row in range(rows):
+            self._getLines(row)
+            self._calc(expression)
+            tband.WriteArray(self.result, 0, row)
+        tband.FlushCache()
+        tband.ComputeStatistics(True)
+        del tband
+        target = None

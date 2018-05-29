e3f52f2d266782bf15ac73e9b463e4621bdd7ceb
agnieszkajaniak <agnieszka.janiakk@gmail.com>
2018-05-29 21:04:45 +0200

    centroid function

--- vector_bootcamp/vector_bootcamp.py
+++ vector_bootcamp/vector_bootcamp.py
@@ -6,7 +6,7 @@ from scipy.spatial import distance as ssd
 from scipy.spatial import distance_matrix
 
 path = os.path.join(os.path.expanduser("~"), "Documents", "pg", "vector_bootcamp")
-pointfile = os.path.join(path, "punkty.shp")
+pointfile = os.path.join(path, "sklepy.shp")
 
 class vect():
     def __init__(self, pointfile):
@@ -16,6 +16,7 @@ class vect():
         projection = layer.GetSpatialRef()
         defn = layer.GetLayerDefn()
         nfields = defn.GetFieldCount()
+        print layer.GetGeomType() == ogr.wkbPoint
 
         fnames = [defn.GetFieldDefn(i).GetName() for i in range(nfields)]
         types = [defn.GetFieldDefn(i).GetTypeName() for i in range(nfields)]
@@ -33,12 +34,12 @@ class vect():
         self.arr_points = self.arr_points[:, 0:2]
         self.dm = distance_matrix(self.arr_points, self.arr_points)
 
-    def createFile(self, type, points_data=None, line_data=None, polygon_data=None):
+    def createFile(self, type, filename, points_data=None, line_data=None, polygon_data=None):
         proj = osr.SpatialReference()
         proj.ImportFromEPSG(2180)
         driver = ogr.GetDriverByName("ESRI Shapefile")
         if (type == "point"):
-            pointFile = driver.CreateDataSource("punkty.shp")
+            pointFile = driver.CreateDataSource(self.name)
             pointLayer = pointFile.CreateLayer("layer", proj, ogr.wkbPoint)
             fieldDef = ogr.FieldDefn("ID", ogr.OFTInteger)
             pointLayer.CreateField(fieldDef)
@@ -54,7 +55,7 @@ class vect():
                 ID += 1
             pointFile = None
         elif (type == "line"):
-            lineFile = driver.CreateDataSource("linie.shp")
+            lineFile = driver.CreateDataSource(name)
             lineLayer = lineFile.CreateLayer("layer", proj, ogr.wkbLineString)
             for row in line_data:
                 feature = ogr.Feature(lineLayer.GetLayerDefn())
@@ -66,7 +67,7 @@ class vect():
                 feature = None
 
         elif (type == "polygon"):
-            polyFile = driver.CreateDataSource("poligon.shp")
+            polyFile = driver.CreateDataSource(name)
             polyLayer = polyFile.CreateLayer("layer", proj, ogr.wkbPolygon)
             polyDef = ogr.FieldDefn("Name", ogr.OFTString)
             polyDef.SetWidth(50)
@@ -84,6 +85,45 @@ class vect():
             feature.Destroy()
             polyFile = None
 
+    def centeroid(self, name):
+        self.name = name
+        mean_x = np.mean(self.arr_points[:, 0])
+        mean_y = np.mean(self.arr_points[:, 1])
+        result = []
+        temp = []
+        temp.append(mean_x)
+        temp.append(mean_y)
+        result.append(temp)
+        result = np.array(result)
+        self.createFile(type='point', filename = self.name, points_data=result)
+
+    def medoid(name):
+        ajdi = np.argmin(data.sum(axis=1))  # to find medoid ID
+        medoid = ogr.FieldDefn("Medoid", ogr.OFTInteger)  # utworzenie nowej kolumny
+        layer.CreateField(medoid)
+        for i in range(len(layer)):
+            if i == ajdi:
+                feature = ogr.Feature(layer.GetLayerDefn())
+                feature.SetField("Medoid", 1)
+                layer.CreateFeature(feature)
+            else:
+                feature = ogr.Feature(layer.GetLayerDefn())
+                feature.SetField("Medoid", 0)
+                layer.CreateFeature(feature)
+            feature = None
+
+    def conhul(bufdist):
+        pointfile = os.path.join(path, namefile)
+        driver = ogr.GetDriverByName("ESRI Shapefile")
+        source = driver.Open(pointfile, 0)
+        layer = source.GetLayer()
+        geomcol = ogr.Geometry(ogr.wkbGeometryCollection)
+        for ef in layer:
+            geomcol.AddGeometry(ef.GetGeometryRef())
+        hull = geomcol.ConvexHull()
+        poly = hull.Buffer(bufdist)
+        print "%s buffered by %d is %s" % (hull.ExportToWkt(), bufdist, poly.ExportToWkt())
+
     def clique(self, dist_matrix):
         triu = np.triu(dist_matrix)
         selected = np.argwhere(np.logical_and(triu < 800, triu > 0))
@@ -97,4 +137,3 @@ class vect():
         self.createFile(type='line', line_data=result)
 
 v = vect(pointfile)
-x = v.clique(v.dm)
